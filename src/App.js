1b3b18247777ef68520cc7bc1b0d68939423b399
Justin Tucker <justin@certainstrings.com>
2019-10-20 11:57:00 -0700

    Clean up app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import './App.css';
-import Title from './components/Title';
-import Options from './components/Options';
-import VehicleInfo from './components/VehicleInfo';
+import React from "react";
+import "./App.css";
+import Title from "./components/Title";
+import Options from "./components/Options";
+import VehicleInfo from "./components/VehicleInfo";
 
-function App (props) {
-  return (
-    <div>
-      <Title title='Welcome to React Transportation' subtitle='The best place to buy vehicles online' />
-      <Options option='Choose Options' />
-      <VehicleInfo 
-        showCars={true}
-        showTrucks={true}
-        showConvertibles={true}
-      />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <Title
+      title="Welcome to React Transportation"
+      subtitle="The best place to buy vehicles online"
+    />
+    <Options option="Choose Options" />
+    <VehicleInfo showCars={true} showTrucks={true} showConvertibles={true} />
+  </div>
+);
 
 export default App;
